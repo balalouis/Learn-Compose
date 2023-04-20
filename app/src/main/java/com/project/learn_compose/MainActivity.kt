177c53babf27c03faaed387d51253b0b137190c0
SBALAMU1 <balan.ssn@gmail.com>
2023-04-20 13:10:18 +0530

    LazyColumn, remember, animateColorAsState

--- app/src/main/java/com/project/learn_compose/MainActivity.kt
+++ app/src/main/java/com/project/learn_compose/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.project.learn_compose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -21,12 +27,21 @@ import com.project.learn_compose.ui.theme.LearnComposeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val messageList = SampleData.conversationSample
         setContent {
             LearnComposeTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    GreetingCard(Message(author = "Arun", body = "Hello World"))
-                }
+                Conversation(messages = messageList)
             }
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>){
+    LazyColumn{
+        items(messages) { message ->
+            Log.i("-----> ","${message.author}")
+            GreetingCard(message)
         }
     }
 }
@@ -46,18 +61,30 @@ fun GreetingCard(msg:Message) {
 
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        var isExpanded by remember { mutableStateOf(false) }
+
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
-                text = "Hello ${msg.author}!",
+                text = msg.author,
                 modifier = Modifier.padding(2.dp),
                 color = MaterialTheme.colors.secondaryVariant,
                 style = MaterialTheme.typography.subtitle2
             )
             Spacer(modifier = Modifier.height(4.dp))
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
                 Text(
-                    text = msg.author,
-                    modifier = Modifier.padding(all=2.dp),
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 2.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.body2
                 )
             }
