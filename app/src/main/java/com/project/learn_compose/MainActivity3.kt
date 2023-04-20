922dab04d787714e6795fbb0fcf3c334726fa770
SBALAMU1 <balan.ssn@gmail.com>
2023-04-20 16:32:21 +0530

    Learn fragment with compose

---
+++ app/src/main/java/com/project/learn_compose/MainActivity3.kt
@@ -0,0 +1,13 @@
+package com.project.learn_compose
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity3 : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
