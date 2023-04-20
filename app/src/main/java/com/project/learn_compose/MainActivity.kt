ec3faf5ce588db8e1455891d63bbdc6e2972de58
SBALAMU1 <balan.ssn@gmail.com>
2023-04-20 11:16:31 +0530

    Added column and row with text, images

--- app/src/main/java/com/project/learn_compose/MainActivity.kt
+++ app/src/main/java/com/project/learn_compose/MainActivity.kt
@@ -3,26 +3,43 @@ package com.project.learn_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val message = Message(author = "Arun", body = "Hello World")
         setContent {
-            GreetingCard("Android")
+            GreetingCard(message)
         }
     }
 }
 
 @Composable
-fun GreetingCard(name: String) {
-    Text(text = "Hello $name!")
+fun GreetingCard(msg:Message) {
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.baby_yellow),
+            contentDescription = "Contact Profile Picture"
+        )
+
+        Column {
+            Text(text = "Hello ${msg.author}!")
+            Text(text = msg.author)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewGreetingCard() {
-    GreetingCard("Android")
+    GreetingCard(Message(author = "Arun", body = "Hello World"))
 }
+
+data class Message(val author:String, val body:String)
