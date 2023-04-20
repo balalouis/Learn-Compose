cab5060c0dccea861d5a0b7b84aca8bf09d12e05
SBALAMU1 <balan.ssn@gmail.com>
2023-04-20 11:50:33 +0530

    Removed Un used import

--- app/src/main/java/com/project/learn_compose/MainActivity.kt
+++ app/src/main/java/com/project/learn_compose/MainActivity.kt
@@ -6,12 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Shapes
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
